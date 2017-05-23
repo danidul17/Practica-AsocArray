f5d9e19e3615cc23c9f6cab59fd755bacaa9d699
danidul17 <danidul17@hotmail.com>
2017-05-23 21:31:29 +0200

    red: creacion del array vacio

--- AsocArrayTest.java
+++ AsocArrayTest.java
@@ -1,5 +1,18 @@
 package Test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import AsocArray.AsocArray;
+
 public class AsocArrayTest {
 
+	private AsocArray array;
+
+	@Test
+	public void creacionDelArray()  {
+		array = new AsocArray();
+		assertEquals(0, array.size());
+	}
 }
